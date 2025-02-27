12e18b58acad83f14e94e94d5deea565cc6715b6
dhwipshah <shahdhwip48@gmail.com>
2025-02-27 16:39:29 +0530

    UI Changes

--- frontend/src/components/Movies/AllMovies.js
+++ frontend/src/components/Movies/AllMovies.js
@@ -5,36 +5,43 @@ import { getAllMovies } from "../../helpers/api-helpers";
 import CradLayout from "../HomePage/CradLayout";
 
 const AllMovies = () => {
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
+
   useEffect(() => {
     getAllMovies()
       .then((data) => setMovies(data))
       .catch((err) => console.log(err));
   }, []);
+
   return (
-    <Box margin="auto" marginTop={4}>
-      <Typography variant="h4" padding={2} textAlign="center">
+    <Box margin="auto" marginTop={4} width="90%">
+      <Typography variant="h4" padding={2} textAlign="center" fontWeight="bold" color="primary">
         All Movies
       </Typography>
       <Box
         margin="auto"
         width="100%"
-        display={"flex"}
-        justifyContent="center"
-        flexWrap={"wrap"}
+        display="grid"
+        gridTemplateColumns={{ xs: "1fr", sm: "1fr 1fr", md: "1fr 1fr 1fr" }}
         gap={4}
+        padding={2}
       >
-        {movies &&
-          movies.map((movie, index) => (
+        {movies.length > 0 ? (
+          movies.map((movie) => (
             <CradLayout
+              key={movie._id}
               id={movie._id}
               title={movie.title}
               releaseDate={movie.releaseDate}
               posterUrl={movie.posterUrl}
               description={movie.description}
-              key={index}
             />
-          ))}
+          ))
+        ) : (
+          <Typography variant="h6" textAlign="center" width="100%">
+            No Movies Available
+          </Typography>
+        )}
       </Box>
     </Box>
   );
